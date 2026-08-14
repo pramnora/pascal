3737314838a1f34ed138191e5c92ecc45ad63180
Paul Ramnora <pramnora@yahoo.com>
2026-08-14 19:06:10 +0100

    Add Pascal program demonstrating data types

---
+++ fpc/language/data-types/data-types-01.pas
@@ -0,0 +1,26 @@
+program dataTypes;
+
+var
+  age: Integer;         // Whole numbers
+  score: Real;          // Floating-point numbers
+  userName: String;     // Text strings
+  isActive: Boolean;    // True or False
+
+begin
+
+  // Initialise variables
+
+  age := 25;
+  score := 1.3;
+  userName := 'Paul';
+  isActive := True;
+
+
+  // Output variables
+
+  writeln(age);
+  writeln(score);
+  writeln(userName);
+  writeln(isActive);
+
+end.
