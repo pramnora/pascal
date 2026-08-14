4565c92725e7f0dcc4cd3faf0b93ab78ad70ffa9
Paul Ramnora <pramnora@yahoo.com>
2026-08-14 20:03:37 +0100

    Add basic arithmetic operations in maths1.pas

---
+++ fpc/language/maths/maths1.pas
@@ -0,0 +1,18 @@
+program mathsOps;
+
+var num1,num2: Integer;
+
+begin
+  write('Enter num 1: ');
+  readln(num1);
+
+  write('Enter num 2: ');
+  readln(num2);
+
+  writeln(); // prints a blank line
+
+  writeln(num1,' + ',num2,' = ',num1 + num2);
+  writeln(num1,' - ',num2,' = ',num1 - num2);
+  writeln(num1,' * ',num2,' = ',num1 * num2);
+  writeln(num1,' / ',num2,' = ',num1 / num2);
+end.
